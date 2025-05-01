2349c986b5ebd2a8c455823aafffd2d26c543788
Lluc Velazquez <lluc.velazquez.7e8@itb.cat>
2025-05-01 13:59:49 +0200

    Created threads, and colors comensal

--- M09.T2.PR1/Program.cs
+++ M09.T2.PR1/Program.cs
@@ -1,10 +1,30 @@
-﻿namespace M09.T2.PR1
+﻿using System.Drawing;
+
+namespace M09.T2.PR1
 {
-    internal class Program
+    public class Program
     {
-        static void Main(string[] args)
+        static readonly ConsoleColor[] COLORS = { ConsoleColor.Blue, ConsoleColor.Red, ConsoleColor.Green, 
+            ConsoleColor.Yellow, ConsoleColor.Cyan };
+        public static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            List<Thread> threads = new();
+            for (int i = 0; i < 5; i++)
+            {
+                int id = i;
+                Thread t = new Thread(() => Comensal(id));
+                threads.Add(t);
+                t.Start();
+            }
+        }
+        public static void Comensal(int id)
+        {
+            ConsoleColor color = COLORS[id];
+            Console.ForegroundColor = color;
+            Console.WriteLine($"Thread {id} started.");
+            Thread.Sleep(1000); // Simulate work
+            Console.WriteLine($"Thread {id} finished.");
+            Console.ResetColor();
         }
     }
 }
