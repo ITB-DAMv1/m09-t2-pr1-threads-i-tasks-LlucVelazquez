3bdba687c55074422db34c6e7894530782684daf
Lluc Velazquez <lluc.velazquez.7e8@itb.cat>
2025-05-01 13:25:50 +0200

    Initial Commit

---
+++ M09.T2.PR1/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace M09.T2.PR1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
