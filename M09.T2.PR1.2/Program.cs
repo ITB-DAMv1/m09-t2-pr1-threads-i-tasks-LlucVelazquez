5508dd912395a8dcc1c64187e47a934fd57c603d
Lluc Velazquez <lluc.velazquez.7e8@itb.cat>
2025-05-10 13:26:11 +0200

    Solution Part 2 Added

---
+++ M09.T2.PR1.2/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace M09.T2.PR1._2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
